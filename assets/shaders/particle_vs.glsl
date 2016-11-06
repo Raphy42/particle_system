8541822ebe5f12092fb25151ce0b53c6d8152748
Raphael Dantzer <rdantzer@student.42.fr>
2016-11-06 18:47:02 +0100

    push

--- assets/shaders/particle_vs.glsl
+++ assets/shaders/particle_vs.glsl
@@ -1,7 +1,5 @@
 #version 410 core
 
-#define POINT_SIZE      0.1f
-
 layout (location = 0) in vec4 pos;
 
 //TODO use uniform buffer object because
@@ -12,6 +10,6 @@ out vec4 color;
 
 void main() {
     gl_Position = mvp * pos;
-    gl_PointSize = max(POINT_SIZE, POINT_SIZE / gl_Position.w);
+    gl_PointSize = gl_Position.z;
     color = gl_Position;
 }
